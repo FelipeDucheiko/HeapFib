9baf70a0c9b4b78d117c7761b1bd051c32a468ac
Felipe31 <felipe31soares@gmail.com>
2015-06-02 10:02:06 -0300

    Função extract incompleta

--- extract.c
+++ extract.c
@@ -1,19 +1,54 @@
 #include "fib.h"
 #include <stdio.h>
-
+#include <assert.h>
 
 HeapFib * makeHeapFib()
 {
-	HeapFib * no = (HeapFib *) malloc(sizeof(heap));
+	HeapFib * no = (HeapFib *) malloc(sizeof(HeapFib));
 	if(!no) return NULL;
 
-	no -> noMin == NULL;
+	no -> noMin = NULL;
 	no -> qtdNos = 0;
 
 	return no;
 
 }
 
+
+NoHeapFib * extractMin(HeapFib * H)
+{
+	assert(H);
+
+	NoHeapFib * ext = H -> noMin;
+	NoHeapFib * filho = ext -> filho;
+	NoHeapFib * aux = NULL;
+
+	if(!ext)
+	{
+		if(!filho)
+		{
+			aux = filho -> esq;
+
+			(ext -> esq) -> dir = filho;
+			filho -> esq = ext -> esq;
+
+			(ext -> dir) -> esq = aux;
+			aux -> dir = ext -> dir;
+
+			while(aux != filho)
+			{
+				aux -> pai	= NULL;
+				aux = aux -> esq;
+			}
+
+			filho -> pai = NULL;
+
+			
+		}
+	}
+
+
+}
 
 
 

51e60f6bfb017d6f46b0d043ec602e45529d4a02
FelipeDuc <felipeducheiko@alunos.utfpr.edu.br>
2015-06-02 09:20:59 -0300

    Arquivo de cabeçalho
    .

---
+++ fib.h
@@ -0,0 +1,29 @@
+#ifndef FIB_H
+#define FIB_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct NoHeapFib{
+
+	struct NoHeapFib* pai; //ponteiro para o nó pai
+	struct NoHeapFib* filho; //ponteiro para um nó filho
+	struct NoHeapFib* esq; //ponteiro para o nó irmão esquerdo
+	struct NoHeapFib* dir; //ponteiro para o nó irmão direito
+	int chave;
+	int grau; //quantidade de filhos do nó
+	int marca; //campo booleano que determina se o nó já 
+
+} NoHeapFib;
+
+
+typedef struct HeapFib
+{
+	NoHeapFib * noMin;
+	int qtdNos;
+} HeapFib;
+
+void insereFib( HeapFib * H,int chave);
+
+
+#endif
